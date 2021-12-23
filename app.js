2d1bd3df3ed95157aa9b7c42407a64fd3e451db0
OktayCopurlu <selimhalim12@gmail.com>
2021-12-23 12:21:55 +0100

    add express

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
+import express from "express";
 import dotenv from "dotenv";
+const app = express();
 dotenv.config();
-const httpServer = createServer();
 const port = process.env.PORT || 4001;
-httpServer.listen(port, () => {
-  console.log("listening on " + port);
-});
 
-const io = new Server(httpServer, {
+const server = createServer(app);
+const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
+server.listen(port, () => {
+  console.log("listening on " + port);
+});
+// httpServer.listen(port, () => {
+//   console.log("listening on " + port);
+// });
+
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
 io.sockets.on("connection", (socket) => {
   socket.on("send-message", (message) => {
     io.sockets.emit("receive-message", message);
